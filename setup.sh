d5315bfe63098740c2fcab16aa249df40d50112a
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-03-19 20:35:02 +0100

    u

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ ! -d "$dir" ]; then
     echo "Directory is empty"
     cd "$dir"
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git .
-    echo 'export COMMANDLINE_ARGS="--listen"' >> ./webui-user.sh
+    echo 'export COMMANDLINE_ARGS="--listen --enable-insecure-extension-access"' >> ./webui-user.sh
     wget -P models/Stable-diffusion https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
 else
     echo "Directory is not empty"
